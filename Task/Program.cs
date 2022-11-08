dbda708a3459b273d59de72289bcd29bd44aab0a
Anastasia <popugaychic@yandex.ru>
2022-11-08 21:57:05 +0500

    created first method

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,3 +2,17 @@
 
 WriteLine("Введите слова через пробел");
 string[] words = ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int length = NewArrayLength(words);
+
+
+int NewArrayLength(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string str = arr[i];
+        int length = str.Length;
+        if (length <= 3) count++;
+    }
+    return count;
+}
